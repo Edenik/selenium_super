4704545b66bbc81c059eea21bc27fe35ead658d7
Eden Nahum <Edenik5@gmail.com>
2022-09-14 02:05:19 +0300

    Utils as classes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='selenium_super',
-    version='1.0.3',
+    version='1.0.4',
     author='Eden Nahum',
     author_email='Edenik5@gmail.com',
     description='Selenium web driver with super abilities',
