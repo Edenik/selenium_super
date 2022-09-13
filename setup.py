e634ddc66e05b8bce4fd42583ab5a974b1b98293
Eden Nahum <Edenik5@gmail.com>
2022-09-14 02:10:33 +0300

    Fix time utils

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='selenium_super',
-    version='1.0.5',
+    version='1.0.6',
     author='Eden Nahum',
     author_email='Edenik5@gmail.com',
     description='Selenium web driver with super abilities',
