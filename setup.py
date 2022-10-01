04225cf2171ddf49bc8517352e690d83e8cb805c
Eden Nahum <Edenik5@gmail.com>
2022-10-01 20:24:31 +0300

    test install

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 import setuptools
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
+def load_requirements(fname):
+    reqs = parse_requirements(fname)
+    return [str(ir.req) for ir in reqs]
+
+
+# with open(lib_folder + '/requirements.txt') as f:
+#     install_requires = f.read().splitlines()
     
 setuptools.setup(
     name='selenium_super',
@@ -21,5 +36,5 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     license='MIT',
-    install_requires=install_requires,
+    install_requires=load_requirements('requirements.txt'),
 )
